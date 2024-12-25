5bd1270a7df7270a705d7d54b37619649dd562f3
danielz0102 <danyzglez@hotmail.com>
2024-12-24 18:18:28 -0600

    Remove unnecesary html-loader package

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,6 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.html$/,
-        use: ["html-loader"],
-      },
-      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       }
